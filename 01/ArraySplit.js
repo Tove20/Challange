d93cbd10f46c30143c46a7af02e94d0b419c3f49
Tove20 <75293012+Tove20@users.noreply.github.com>
2022-08-01 23:47:05 +0200

    Update ArraySplit.js

--- 01/ArraySplit.js
+++ 01/ArraySplit.js
@@ -1,12 +1,13 @@
 const arr = require("./Arr1.js")
-
-//const arr = elements;
 const result = new Array;
 var size = 3;
 
 for (let i = 0;  i < arr.length; i += size) {
   result.push(arr.slice(i, i + size))
 }
+
+Consol.log(result.slice(0, 2));
+
 
 const arraySplit = result.slice(0, 2);
 
